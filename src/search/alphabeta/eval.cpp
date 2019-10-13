fe4f6162c69acb25affbce18b5090929628a1732
kz04px <6536046+kz04px@users.noreply.github.com>
2019-10-13 19:10:15 +0100

    Add PST to eval
    248 - 152 - 0
    84.60 +/- 34.97

--- src/search/alphabeta/eval.cpp
+++ src/search/alphabeta/eval.cpp
@@ -5,11 +5,35 @@ namespace search {
 
 namespace alphabeta {
 
+// clang-format off
+constexpr int pst[49] = {
+    30, 20, 10, 10, 10, 20, 30,
+    20, 10, 10,  5, 10, 10, 20,
+    10, 10,  5,  0,  5, 10, 10,
+    10,  5,  0,  0,  0,  5, 10,
+    10, 10,  5,  0,  5, 10, 10,
+    20, 10, 10,  5, 10, 10, 20,
+    30, 20, 10, 10, 10, 20, 30,
+};
+// clang-format on
+
 // Return the evaluation of the position from the side to move's point of view
 int Alphabeta::eval(const libataxx::Position &pos) noexcept {
-    const int num_us = pos.us().count();
-    const int num_them = pos.them().count();
-    return 100 * (num_us - num_them);
+    int score = 0;
+
+    // Material
+    score += 100 * pos.us().count();
+    score -= 100 * pos.them().count();
+
+    // PST
+    for (const auto &sq : pos.us()) {
+        score += pst[static_cast<int>(sq)];
+    }
+    for (const auto &sq : pos.them()) {
+        score -= pst[static_cast<int>(sq)];
+    }
+
+    return score;
 }
 
 }  // namespace alphabeta
