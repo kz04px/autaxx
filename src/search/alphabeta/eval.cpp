87c83de2ad1e7f8e940db71d25f202361947b9af
kz04px <6536046+kz04px@users.noreply.github.com>
2019-10-14 20:27:14 +0100

    Add hole penalties
    244 - 56 - 0
    256 +/- 50.49

--- src/search/alphabeta/eval.cpp
+++ src/search/alphabeta/eval.cpp
@@ -17,6 +17,8 @@ constexpr int pst[49] = {
 };
 // clang-format on
 
+constexpr int hole_penalties[] = {0, 0, 0, 0, 100, 200, 300, 400, 500};
+
 // Return the evaluation of the position from the side to move's point of view
 int Alphabeta::eval(const libataxx::Position &pos) noexcept {
     int score = 0;
@@ -31,6 +33,20 @@ int Alphabeta::eval(const libataxx::Position &pos) noexcept {
     }
     for (const auto &sq : pos.them()) {
         score -= pst[static_cast<int>(sq)];
+    }
+
+    const auto reachable_them = pos.them().singles() | pos.them().doubles();
+    for (const auto &sq : pos.us().singles() & pos.empty() & reachable_them) {
+        const auto bb = libataxx::Bitboard{sq};
+        const auto neighbours = bb.singles() & pos.us();
+        score -= hole_penalties[neighbours.count()];
+    }
+
+    const auto reachable_us = pos.us().singles() | pos.us().doubles();
+    for (const auto &sq : pos.them().singles() & pos.empty() & reachable_us) {
+        const auto bb = libataxx::Bitboard{sq};
+        const auto neighbours = bb.singles() & pos.them();
+        score += hole_penalties[neighbours.count()];
     }
 
     return score;
