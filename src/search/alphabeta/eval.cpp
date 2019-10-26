e0d7f6ee32011640e44654e69e518e0378f892c6
kz04px <6536046+kz04px@users.noreply.github.com>
2019-10-26 18:39:16 +0100

    Reorganise eval

--- src/search/alphabeta/eval.cpp
+++ src/search/alphabeta/eval.cpp
@@ -19,35 +19,36 @@ constexpr int pst[49] = {
 
 constexpr int hole_penalties[] = {0, 0, 0, 0, 100, 200, 300, 400, 500};
 
+[[nodiscard]] constexpr int eval_us(const libataxx::Bitboard &us,
+                                    const libataxx::Bitboard &them,
+                                    const libataxx::Bitboard &empty) {
+    int score = 0;
+
+    // Material
+    score += 100 * us.count();
+
+    // PST
+    for (const auto &sq : us) {
+        score += pst[static_cast<int>(sq)];
+    }
+
+    // Holes
+    const auto reachable_us = us.singles() | us.doubles();
+    for (const auto &sq : them.singles() & empty & reachable_us) {
+        const auto bb = libataxx::Bitboard{sq};
+        const auto neighbours = bb.singles() & them;
+        score += hole_penalties[neighbours.count()];
+    }
+
+    return score;
+}
+
 // Return the evaluation of the position from the side to move's point of view
 int Alphabeta::eval(const libataxx::Position &pos) noexcept {
     int score = 0;
 
-    // Material
-    score += 100 * pos.us().count();
-    score -= 100 * pos.them().count();
-
-    // PST
-    for (const auto &sq : pos.us()) {
-        score += pst[static_cast<int>(sq)];
-    }
-    for (const auto &sq : pos.them()) {
-        score -= pst[static_cast<int>(sq)];
-    }
-
-    const auto reachable_them = pos.them().singles() | pos.them().doubles();
-    for (const auto &sq : pos.us().singles() & pos.empty() & reachable_them) {
-        const auto bb = libataxx::Bitboard{sq};
-        const auto neighbours = bb.singles() & pos.us();
-        score -= hole_penalties[neighbours.count()];
-    }
-
-    const auto reachable_us = pos.us().singles() | pos.us().doubles();
-    for (const auto &sq : pos.them().singles() & pos.empty() & reachable_us) {
-        const auto bb = libataxx::Bitboard{sq};
-        const auto neighbours = bb.singles() & pos.them();
-        score += hole_penalties[neighbours.count()];
-    }
+    score += eval_us(pos.us(), pos.them(), pos.empty());
+    score -= eval_us(pos.them(), pos.us(), pos.empty());
 
     return score;
 }
