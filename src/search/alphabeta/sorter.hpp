b856ef5249e78e377a9c6fdac8dc1499632a611f
kz04px <6536046+kz04px@users.noreply.github.com>
2019-10-29 03:23:02 +0000

    Add staged movegen
    200ms
    530 - 494 - 0
    12.22 +/- 21.29

--- src/search/alphabeta/sorter.hpp
+++ src/search/alphabeta/sorter.hpp
@@ -20,22 +20,38 @@ constexpr int pst[49] = {
 };
 // clang-format on
 
+enum class Stage : std::uint8_t
+{
+    TT = 0,
+    Killer,
+    Captures,
+    Noncaptures,
+    Done
+};
+
 class Sorter {
    public:
     Sorter(const libataxx::Position &pos,
            const libataxx::Move &ttmove,
-           const libataxx::Move &killer) {
-        num_moves_ = pos.legal_moves(moves_);
-        score(pos, ttmove, killer);
+           const libataxx::Move &killer)
+        : pos_{pos},
+          ttmove_{ttmove},
+          killer_{killer},
+          num_moves_{0},
+          moves_{},
+          stage_{Stage::TT} {
     }
 
     [[nodiscard]] bool next(libataxx::Move &move) noexcept {
         if (num_moves_ == 0) {
-            return false;
+            next_stage();
+            if (num_moves_ == 0) {
+                return false;
+            }
         }
 
         int idx = 0;
-        for (int i = 0; i < num_moves_; ++i) {
+        for (int i = 1; i < num_moves_; ++i) {
             if (scores_[i] > scores_[idx]) {
                 idx = i;
             }
@@ -43,36 +59,90 @@ class Sorter {
 
         move = moves_[idx];
 
-        scores_[idx] = scores_[num_moves_ - 1];
         moves_[idx] = moves_[num_moves_ - 1];
+        scores_[idx] = scores_[num_moves_ - 1];
         num_moves_--;
 
         return true;
     }
 
    private:
-    void score(const libataxx::Position &pos,
-               const libataxx::Move &ttmove,
-               const libataxx::Move &killer) noexcept {
-        if (num_moves_ <= 1) {
-            return;
+    void next_stage() noexcept {
+        if (stage_ == Stage::TT) {
+            stage_ = Stage::Killer;
+
+            if (ttmove_ && pos_.legal_move(ttmove_)) {
+                moves_[0] = ttmove_;
+                num_moves_ = 1;
+                return;
+            }
         }
 
-        for (int i = 0; i < num_moves_; ++i) {
-            if (moves_[i] == ttmove) {
-                scores_[i] = 10000;
-                continue;
-            } else if (moves_[i] == killer) {
-                scores_[i] = 9999;
-                continue;
+        if (stage_ == Stage::Killer) {
+            stage_ = Stage::Captures;
+
+            if (killer_ && killer_ != ttmove_ && pos_.legal_move(killer_)) {
+                moves_[0] = killer_;
+                num_moves_ = 1;
+                return;
+            }
+        }
+
+        if (stage_ == Stage::Captures) {
+            stage_ = Stage::Noncaptures;
+
+            num_moves_ = pos_.legal_captures(moves_);
+
+            // Scores
+            for (int i = num_moves_ - 1; i >= 0; --i) {
+                assert(moves_[i] != libataxx::Move::nullmove());
+                assert(moves_[i] != libataxx::Move::nomove());
+                assert(pos_.count_captures(moves_[i]));
+
+                if (moves_[i] == ttmove_ || moves_[i] == killer_) {
+                    moves_[i] = moves_[num_moves_ - 1];
+                    scores_[i] = scores_[num_moves_ - 1];
+                    num_moves_--;
+                    continue;
+                }
+
+                const auto captures = pos_.count_captures(moves_[i]);
+                scores_[i] = 100 * captures + (moves_[i].is_single() ? 100 : 0);
             }
 
-            const auto captures = pos.count_captures(moves_[i]);
+            if (num_moves_ > 0) {
+                return;
+            }
+        }
 
-            if (captures) {
-                scores_[i] = 100 * captures;
-                scores_[i] += moves_[i].is_single() ? 100 : 0;
-            } else {
+        if (stage_ == Stage::Noncaptures) {
+            stage_ = Stage::Done;
+
+            num_moves_ = pos_.legal_noncaptures(moves_);
+
+            if (num_moves_ == 1 && moves_[0] == libataxx::Move::nullmove()) {
+                if (killer_ == libataxx::Move::nullmove() ||
+                    ttmove_ == libataxx::Move::nullmove()) {
+                    num_moves_ = 0;
+                }
+                return;
+            }
+
+            // Scores
+            for (int i = num_moves_ - 1; i >= 0; --i) {
+                assert(moves_[i] != libataxx::Move::nullmove());
+                assert(moves_[i] != libataxx::Move::nomove());
+                assert(static_cast<int>(moves_[i].to()) < 49);
+                assert(static_cast<int>(moves_[i].from()) < 49);
+                assert(!pos_.count_captures(moves_[i]));
+
+                if (moves_[i] == ttmove_ || moves_[i] == killer_) {
+                    moves_[i] = moves_[num_moves_ - 1];
+                    scores_[i] = scores_[num_moves_ - 1];
+                    num_moves_--;
+                    continue;
+                }
+
                 if (moves_[i].is_single()) {
                     scores_[i] = pst[static_cast<int>(moves_[i].to())];
                 } else {
@@ -80,12 +150,21 @@ class Sorter {
                                  pst[static_cast<int>(moves_[i].from())];
                 }
             }
+
+            if (num_moves_ > 0) {
+                return;
+            }
         }
     }
 
+   private:
+    libataxx::Position pos_;
+    libataxx::Move ttmove_;
+    libataxx::Move killer_;
     int num_moves_;
     libataxx::Move moves_[libataxx::max_moves];
     int scores_[libataxx::max_moves];
+    Stage stage_;
 };
 
 }  // namespace alphabeta
---
+++ tests/alphabeta_sorter.cpp
@@ -0,0 +1,111 @@
+#include <catch.hpp>
+#include <cstdint>
+#include <libataxx/move.hpp>
+#include <libataxx/position.hpp>
+#include <vector>
+#include "../src/search/alphabeta/sorter.hpp"
+#include "../src/utils.hpp"
+
+std::uint64_t perft(const libataxx::Position &pos, const int depth) {
+    if (depth == 0) {
+        return 1;
+    }
+
+    libataxx::Move ttmove;
+    libataxx::Move killer;
+
+    {
+        libataxx::Move moves[libataxx::max_moves];
+        const int num_moves = pos.legal_moves(moves);
+
+        ttmove = moves[utils::rand_u32(0, num_moves)];
+        killer = moves[utils::rand_u32(0, num_moves)];
+
+        // ttmove and killer should occasionally be illegal moves
+        const auto a = utils::rand_u32(0, 19);
+        if (a == 0) {
+            ttmove = libataxx::Move::nomove();
+        } else if (a == 1) {
+            ttmove = libataxx::Move::nullmove();
+        } else if (a == 2) {
+            const auto file = libataxx::File(utils::rand_u32(0, 6));
+            const auto rank = libataxx::Rank(utils::rand_u32(0, 6));
+            const auto sq = libataxx::Square(file, rank);
+            const auto move = libataxx::Move(sq);
+            ttmove = move;
+        }
+
+        const auto b = utils::rand_u32(0, 19);
+        if (b == 0) {
+            killer = libataxx::Move::nomove();
+        } else if (b == 1) {
+            killer = libataxx::Move::nullmove();
+        } else if (b == 2) {
+            const auto file = libataxx::File(utils::rand_u32(0, 6));
+            const auto rank = libataxx::Rank(utils::rand_u32(0, 6));
+            const auto sq = libataxx::Square(file, rank);
+            const auto move = libataxx::Move(sq);
+            killer = move;
+        }
+    }
+
+    auto sorter = search::alphabeta::Sorter{pos, ttmove, killer};
+    libataxx::Move move;
+    std::uint64_t nodes = 0;
+
+    const bool tt_legal = pos.legal_move(ttmove);
+    const bool killer_legal = pos.legal_move(killer);
+
+    const bool tt_first = tt_legal;
+    const bool killer_first =
+        (tt_first && ttmove == killer) || (!tt_first && killer_legal);
+    const bool killer_second = tt_first && killer_legal && ttmove != killer;
+
+    int i = 0;
+    while (sorter.next(move)) {
+        switch (i) {
+            case 0:
+                REQUIRE((!tt_first || move == ttmove));
+                REQUIRE((!killer_first || move == killer));
+                break;
+            case 1:
+                REQUIRE((!killer_second || move == killer));
+                break;
+            default:
+                REQUIRE(move != ttmove);
+                REQUIRE(move != killer);
+                break;
+        }
+
+        libataxx::Position npos = pos;
+        npos.makemove(move);
+        nodes += perft(npos, depth - 1);
+        i++;
+    }
+
+    REQUIRE(i == pos.count_moves());
+
+    return nodes;
+}
+
+TEST_CASE("Alphabeta sorter") {
+    const std::pair<std::string, std::vector<std::uint64_t>> tests[] = {
+        {"7/7/7/7/7/7/7 x 0 1", {1, 0, 0, 0, 0}},
+        {"7/7/7/7/7/7/7 o 0 1", {1, 0, 0, 0, 0}},
+        {"x5o/7/7/7/7/7/o5x x 0 1", {1, 16, 256, 6460, 155888, 4752668}},
+        {"x5o/7/7/7/7/7/o5x o 0 1", {1, 16, 256, 6460, 155888, 4752668}},
+        {"x5o/7/2-1-2/7/2-1-2/7/o5x x 0 1", {1, 14, 196, 4184, 86528, 2266352}},
+        {"x5o/7/2-1-2/7/2-1-2/7/o5x o 0 1", {1, 14, 196, 4184, 86528, 2266352}},
+        {"7/7/7/7/ooooooo/ooooooo/xxxxxxx x 0 1", {1, 1, 75, 249, 14270}},
+        {"7/7/7/7/ooooooo/ooooooo/xxxxxxx o 0 1", {1, 75, 249, 14270, 452980}},
+        {"7/7/7/2x1o2/7/7/7 x 0 1", {1, 23, 419, 7887, 168317, 4266992}},
+        {"7/7/7/2x1o2/7/7/7 o 0 1", {1, 23, 419, 7887, 168317, 4266992}},
+    };
+
+    for (const auto &[fen, nodes] : tests) {
+        libataxx::Position pos{fen};
+        for (int i = 0; i < nodes.size(); ++i) {
+            REQUIRE(nodes[i] == perft(pos, i));
+        }
+    }
+}
