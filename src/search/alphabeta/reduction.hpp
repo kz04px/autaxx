be082db986737254301b8939833f0731744d3521
kz04px <6536046+kz04px@users.noreply.github.com>
2019-12-03 21:24:15 +0000

    LMR improvement

--- src/search/alphabeta/reduction.hpp
+++ src/search/alphabeta/reduction.hpp
@@ -1,6 +1,7 @@
 #ifndef SEARCH_ALPHABETA_REDUCTION_HPP
 #define SEARCH_ALPHABETA_REDUCTION_HPP
 
+#include <cassert>
 #include <libataxx/position.hpp>
 
 namespace search {
@@ -11,7 +12,16 @@ int reduction(const libataxx::Position &npos,
               const int move_num,
               const int depth,
               const bool pvnode) {
-    return move_num < 3 ? 0 : 2;
+    assert(depth >= 0);
+    assert(move_num >= 0);
+
+    if (move_num < 3) {
+        return 0;
+    } else if (move_num < 16) {
+        return 2;
+    }
+
+    return 4;
 }
 
 }  // namespace alphabeta
