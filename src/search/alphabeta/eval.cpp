adba74f24f2a07a37203a2d322257aae2bfacba9
kz04px <6536046+kz04px@users.noreply.github.com>
2019-12-09 16:17:07 +0000

    Add tapered eval

--- src/search/alphabeta/eval.cpp
+++ src/search/alphabeta/eval.cpp
@@ -1,31 +1,70 @@
 #include <libataxx/position.hpp>
 #include "alphabeta.hpp"
+#include "phase.hpp"
 
 namespace search {
 
 namespace alphabeta {
 
+constexpr std::pair<int, int> operator+(const std::pair<int, int> &x,
+                                        const std::pair<int, int> &y) {
+    return std::make_pair(x.first + y.first, x.second + y.second);
+}
+
+constexpr std::pair<int, int> operator*(const std::pair<int, int> &x,
+                                        const int n) {
+    return std::make_pair(x.first * n, x.second * n);
+}
+
+constexpr std::pair<int, int> operator+=(std::pair<int, int> &x,
+                                         const std::pair<int, int> &y) {
+    x.first += y.first;
+    x.second += y.second;
+    return x;
+}
+
+constexpr std::pair<int, int> operator-=(std::pair<int, int> &x,
+                                         const std::pair<int, int> &y) {
+    x.first -= y.first;
+    x.second -= y.second;
+    return x;
+}
+
 // clang-format off
-constexpr int pst[49] = {
-    60, 40, 20, 20, 20, 40, 60,
-    40, 20, 10, 10, 10, 20, 40,
-    20, 10,  0,  0,  0, 10, 20,
-    20, 10,  0,  0,  0, 10, 20,
-    20, 10,  0,  0,  0, 10, 20,
-    40, 20, 10, 10, 10, 20, 40,
-    60, 40, 20, 20, 20, 40, 60,
+constexpr std::pair<int, int> pst[49] = {
+    {60,60}, {40,40}, {20,20}, {20,20}, {20,20}, {40,40}, {60,60},
+    {40,40}, {20,20}, {10,10}, {10,10}, {10,10}, {20,20}, {40,40},
+    {20,20}, {10,10},   {0,0},   {0,0},   {0,0}, {10,10}, {20,20},
+    {20,20}, {10,10},   {0,0},   {0,0},   {0,0}, {10,10}, {20,20},
+    {20,20}, {10,10},   {0,0},   {0,0},   {0,0}, {10,10}, {20,20},
+    {40,40}, {20,20}, {10,10}, {10,10}, {10,10}, {20,20}, {40,40},
+    {60,60}, {40,40}, {20,20}, {20,20}, {20,20}, {40,40}, {60,60},
 };
 // clang-format on
 
-constexpr int hole_penalties[] = {0, 0, 25, 50, 100, 200, 300, 400, 500};
+constexpr std::pair<int, int> piece_value = {100, 100};
+constexpr std::pair<int, int> surrounded = {15, 15};
+constexpr std::pair<int, int> turn_bonus = {200, 200};
+constexpr std::pair<int, int> hole_penalties[] = {
+    {0, 0},
+    {0, 0},
+    {25, 25},
+    {50, 50},
+    {100, 100},
+    {200, 200},
+    {300, 300},
+    {400, 400},
+    {500, 500},
+};
 
-[[nodiscard]] constexpr int eval_us(const libataxx::Bitboard &us,
-                                    const libataxx::Bitboard &them,
-                                    const libataxx::Bitboard &empty) {
-    int score = 0;
+[[nodiscard]] constexpr std::pair<int, int> eval_us(
+    const libataxx::Bitboard &us,
+    const libataxx::Bitboard &them,
+    const libataxx::Bitboard &empty) {
+    std::pair<int, int> score;
 
     // Material
-    score += 100 * us.count();
+    score += piece_value * us.count();
 
     // PST
     for (const auto &sq : us) {
@@ -40,22 +79,23 @@ constexpr int hole_penalties[] = {0, 0, 25, 50, 100, 200, 300, 400, 500};
     }
 
     // Surrounded
-    score += 15 * (us & (~(empty.singles()))).count();
+    score += surrounded * (us & (~(empty.singles()))).count();
 
     return score;
 }
 
 // Return the evaluation of the position from the side to move's point of view
 int Alphabeta::eval(const libataxx::Position &pos) noexcept {
-    int score = 0;
+    const auto p = phase(pos);
+    std::pair<int, int> score;
 
     score += eval_us(pos.us(), pos.them(), pos.empty());
     score -= eval_us(pos.them(), pos.us(), pos.empty());
 
     // Turn bonus
-    score += 200;
+    score += turn_bonus;
 
-    return score;
+    return score.first * (1.0 - p) + score.second * p;
 }
 
 }  // namespace alphabeta
