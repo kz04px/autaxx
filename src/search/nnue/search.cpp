a7497c421069b1b5f6b68c44ee37584295e7dd91
kz04px <6536046+kz04px@users.noreply.github.com>
2021-02-26 02:21:14 +0000

    Fix some warnings

--- src/search/nnue/search.cpp
+++ src/search/nnue/search.cpp
@@ -33,8 +33,7 @@ int NNUE::search(Stack *stack, const libataxx::Position &pos, int alpha, int bet
     stats_.seldepth = std::max(stack->ply, stats_.seldepth);
 
     // Return mate or draw scores if the game is over
-    const auto r = pos.result();
-    if (r != libataxx::Result::None) {
+    if (const auto r = pos.result(); r != libataxx::Result::None) {
         if (r == libataxx::Result::Draw) {
             return 0;
         } else if (r == libataxx::Result::BlackWin) {
