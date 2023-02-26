4b41f452fbe36a3f8f7ccb0a0d02139336581b04
kz04px <6536046+kz04px@users.noreply.github.com>
2023-02-26 13:12:48 +0000

    Print position hash in hex

--- src/autaxx/protocol/common/display.cpp
+++ src/autaxx/protocol/common/display.cpp
@@ -7,7 +7,7 @@ namespace common {
 void display(const libataxx::Position &pos) {
     std::cout << pos << std::endl;
     std::cout << pos.get_fen() << std::endl;
-    std::cout << pos.hash() << std::endl;
+    std::cout << std::hex << "0x" << pos.hash() << std::dec << std::endl;
 }
 
 }  // namespace common
